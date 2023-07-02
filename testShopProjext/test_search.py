c01f1b3b362c78f583f25af91a6460df494c4d20
Nitablack <nitablack@rambler.ru>
2023-07-02 17:46:52 +0500

    searchTest
    searchTest

---
+++ testShopProjext/test_search.py
@@ -0,0 +1,19 @@
+import pytest  # подключаем библиотеку pytest
+from selenium import webdriver  # подключаем библиотеку selenium
+from pages.search import SearchPage  # добавляем PageObject wiki
+
+driver = webdriver.Chrome()  # определяем переменную для webdriver
+search = SearchPage(driver)  # определяем переменную для PageObject и делаем объект класса нашей страницы
+
+@pytest.fixture() # определяем фикстуру pre_test,
+def pre_test(request): # в которой вызываем функцию
+    search.load()        # загрузки страницы нашего PageObject
+
+def teardown_module(module): # делаем фикстуру закрытия браузера,
+    driver.close()           # которая будет выполняться после всех тестов
+
+def test_search_car(pre_test): # поиск с основной страницы
+    search.search('car')
+
+def test_search_abc():  # повторный поиск
+    search.search('яблоко')
